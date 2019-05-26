19b606d45787b8e9a330e5b79ac57db017c5d765
Emre AKDAŞ <eemreakdas@gmail.com>
2019-05-26 22:20:46 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// Değişkene göre kendiniz düzenleyebilirsiniz.
+        include 'app/Libraries/Shopier/shopierAPI.php';
+				$shopierKey = $pos['shopierKey'];
+				$shopierSecret = $pos['shopierSecret'];
+				$shopier = new Shopier($shopierKey, $shopierSecret);
+				$shopier->setBuyer([
+					'id' => $kods,
+					'paket' => $paketbaslik,
+					'first_name' => $ad, 'last_name' => $soyad, 'email' => $email, 'phone' => $telefon]);
+				$shopier->setOrderBilling([
+					'billing_address' => $acikadres,
+					'billing_city' => $il,
+					'billing_country' => $ulke,
+					'billing_postcode' => $postakodu,
+				]);
+				$shopier->setOrderShipping([
+					'shipping_address' => $acikadres,
+					'shipping_city' => $il,
+					'shipping_country' => $ulke,
+					'shipping_postcode' => $postakodu,
+				]);
+				die($shopier->run($paketid, $fiyat, $callback));
+?>
