f5d61f74a1ed735c694e9736b42d8d60b5f8f7ed
Emre AKDAŞ <eemreakdas@gmail.com>
2019-06-08 13:38:09 +0300

    Create shopierNotify.php

---
+++ shopierNotify.php
@@ -0,0 +1,29 @@
+<?php
+//Shopierdan gelen postlar.
+$status = $_POST["status"];
+$invoiceId = $_POST["platform_order_id"];
+$transactionId = $_POST["payment_id"];
+$installment = $_POST["installment"];
+$signature = $_POST["signature"];
+
+/* Bu kısımda kullanıcının işlem başarılı ve başarısızsa yönleneceği değişkenlerdir. */
+$url = 'https://emreakdas.com/';
+$locationtrue = $url."order?orderNo=$invoiceId";
+$locationfalse = $url."order?orderNo=none";
+/* Bu kısımda kullanıcının işlem başarılı ve başarısızsa yönleneceği değişkenlerdir. */
+
+$data = $_POST["random_nr"] . $_POST["platform_order_id"] . $_POST["total_order_value"] . $_POST["currency"];
+$signature = base64_decode($signature);
+$expected = hash_hmac('SHA256', $data, $shopierSecret, true);
+if ($signature == $expected) {
+$status = strtolower($status);
+if ($status == "success") {
+//İşlem başarılı ise yapacaklarınız
+header("Location: $locationtrue");
+}
+else{
+// İşlem başarısız ise yapacaklarınız.
+header("Location: $locationfalse");
+}
+}
+?>
