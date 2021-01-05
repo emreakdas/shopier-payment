c5e43a23a2c710e93d24b595cc135181d95579b4
bgokcol <47295517+bgokcol@users.noreply.github.com>
2021-01-05 12:01:08 +0300

    Update shopierNotify.php

--- shopierNotify.php
+++ shopierNotify.php
@@ -12,7 +12,7 @@ $locationtrue = $url."order?orderNo=$invoiceId";
 $locationfalse = $url."order?orderNo=none";
 /* Bu kısımda kullanıcının işlem başarılı ve başarısızsa yönleneceği değişkenlerdir. */
 
-$data = $_POST["random_nr"] . $_POST["platform_order_id"] . $_POST["total_order_value"] . $_POST["currency"];
+$data = $_POST["random_nr"] . $_POST["platform_order_id"];
 $signature = base64_decode($signature);
 $expected = hash_hmac('SHA256', $data, $shopierSecret, true);
 if ($signature == $expected) {
